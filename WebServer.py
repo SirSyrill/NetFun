54d8ef802a14d3d84e67fb711202e6f025a64131
SirSyrill <maxosyka.fe@gmail.com>
2025-08-26 17:49:56 +1000

    fixed minor error such as typos or missplaced index. fixed 200 OK request by adding content type and trailing line.

--- WebServer.py
+++ WebServer.py
@@ -1,35 +1,26 @@
 #import socket module
 from socket import *
+import sys
 serverSocket = socket(AF_INET, SOCK_STREAM)
 #Prepare a sever socket
-#Fill in start
 serverPort = 6789  # note: this can be changed if needed
 serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 print(f"Listening on http://localhost:{serverPort}")
-#Fill in end
 while True:
 #Establish the connection
     print('Ready to serve...')
-    #Fill in start 
     connectionSocket, addr = serverSocket.accept()           
-     #Fill in end
     try:
-        message = #Fill in start
-        connectionsocket.recv(290).decode() 
-        print("recieve request :/n", message)
-        #Fill in end
-        filename = message.split()1
+        message = connectionSocket.recv(1024).decode() 
+        print("receive request :/n", message)
+        filename = message.split()[1]
         f = open(filename[1:])
-        outputdata = #Fill in start
-        f.read()
-        #Fill in end
+        outputdata = f.read()
 
         #Send one HTTP header line into socket
-        #Fill in start
-        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
-        #Fill in end
+        connectionSocket.send("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n".encode()) #was missing content type and another set opf \r\n at the end
 
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
@@ -38,14 +29,11 @@ while True:
         connectionSocket.close()
     except IOError:
         #Send response message for file not found
-        #Fill in start
         connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
         connectionSocket.send("<html><body><h1>404 Not Found</h1></body></html>".encode())
-        #Fill in end
 
         #Close client socket
-        #Fill in start
         connectionSocket.close()
-        #Fill in end
+    ##break  <--- if we want a singe connection
 serverSocket.close()
 sys.exit()#Terminate the program after sending the corresponding data
